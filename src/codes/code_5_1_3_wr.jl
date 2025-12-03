4a1e0489bbe5aec2f1c4d735dc70520e2e883865
suying liu <syliu@umd.edu>
2025-12-02 21:19:02 -0500

    Add two-local penalty Hamiltonian

--- src/codes/code_5_1_3_wr.jl
+++ src/codes/code_5_1_3_wr.jl
@@ -1,3 +1,78 @@
 # Definitions for the weight-reduced (WR) [[5,1,3]] code
+# Stabilizer space is the zero-energy eigenspace of the penalty Hamiltonian
 
-# TODO
+export Code_5_1_3_wr
+
+struct Code_5_1_3_wr <: AbstractCode
+    num_data_qubits::Int
+    num_logi_qubits::Int
+    Code_5_1_3_wr() = new(5, 1)
+end
+
+
+function stabilizer_generators(::Code_5_1_3_wr)
+    stabs = [
+        X ⊗ Z ⊗ Z ⊗ X ⊗ I,
+        I ⊗ X ⊗ Z ⊗ Z ⊗ X,
+        X ⊗ I ⊗ X ⊗ Z ⊗ Z,
+        Z ⊗ X ⊗ I ⊗ X ⊗ Z
+    ]
+    return stabs
+end
+
+
+function stabilizer_matrix(::Code_5_1_3_wr)
+    Mx = @SMatrix [
+        1 0 0 1 0;
+        0 1 0 0 1;
+        1 0 1 0 0;
+        0 1 0 1 0
+    ]
+    Mz = @SMatrix [
+        0 1 1 0 0;
+        0 0 1 1 0;
+        0 0 0 1 1;
+        1 0 0 0 1
+    ]
+    return Mx, Mz
+end
+
+
+function logical_Zs(::Code_5_1_3_wr)
+    return [Z ⊗ Z ⊗ Z ⊗ Z ⊗ Z]
+end
+
+
+function logical_Xs(::Code_5_1_3_wr)
+    return [X ⊗ X ⊗ X ⊗ X ⊗ X]
+end
+
+
+function encoding_isometry(::Code_5_1_3_wr)
+    ψ0 = 1 / 4 * ((@ket_from_bits "00000") + (@ket_from_bits "10010") + (@ket_from_bits "01001") +
+                  (@ket_from_bits "10100") + (@ket_from_bits "01010") - (@ket_from_bits "11011") -
+                  (@ket_from_bits "00110") - (@ket_from_bits "11000") - (@ket_from_bits "11101") -
+                  (@ket_from_bits "00011") - (@ket_from_bits "11110") - (@ket_from_bits "01111") -
+                  (@ket_from_bits "10001") - (@ket_from_bits "01100") - (@ket_from_bits "10111") +
+                  (@ket_from_bits "00101"))
+    ψ1 = (X ⊗ X ⊗ X ⊗ X ⊗ X) * ψ0
+    Uenc_data = [ψ0.data ψ1.data]
+    Uenc_dims = GeneralDimensions((ntuple(_ -> 2, Val(5)), ntuple(_ -> 2, Val(1))))
+    Uenc = QuantumObject(Uenc_data, dims=Uenc_dims)
+    return Uenc
+end
+
+
+function penalty_hamiltonian(::Code_5_1_3_wr)
+    # Hpen = -1. * (X ⊗ Z ⊗ Z ⊗ X ⊗ I +
+    #               I ⊗ X ⊗ Z ⊗ Z ⊗ X +
+    #               X ⊗ I ⊗ X ⊗ Z ⊗ Z +
+    #               Z ⊗ X ⊗ I ⊗ X ⊗ Z +
+    #               Z ⊗ Z ⊗ X ⊗ I ⊗ X)
+    Hpen =  -2. * (X ⊗ Z ⊗ I ⊗ I ⊗ I) + 2. * (I ⊗ I ⊗ Z ⊗ X ⊗ I)
+            + -2. * (I ⊗ X ⊗ I ⊗ Z ⊗ I) + 2. * (I ⊗ I ⊗ Z ⊗ I ⊗ X)
+            + -2. * (X ⊗ I ⊗ X ⊗ I ⊗ I) + 2. * (I ⊗ I ⊗ I ⊗ Z ⊗ Z)
+            + -1. * (Z ⊗ X ⊗ I ⊗ I ⊗ I) + 1. * (I ⊗ I ⊗ I ⊗ X ⊗ Z)
+            + -1. * (Z ⊗ I ⊗ I ⊗ X ⊗ I) + 1. * (I ⊗ X ⊗ I ⊗ I ⊗ Z)
+    return Hpen
+end
